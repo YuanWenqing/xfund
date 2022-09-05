322a8045c44a2a828ff13901cb866ccf60c92d47
yuan <yuan@preferr.com>
2022-09-05 17:29:56 +0800

    repr

--- fundstrategy/fund_apis/__init__.py
+++ fundstrategy/fund_apis/__init__.py
@@ -1,1 +1,2 @@
 # coding: utf8
+from .doctorxiong import DoctorXiong
--- fundstrategy/fund_apis/models.py
+++ fundstrategy/fund_apis/models.py
@@ -7,6 +7,9 @@ class FundInfo:
         self.code = code
         self.name = name
 
+    def __repr__(self):
+        return f'<{self.__class__.__name__}: {self.code}, {self.name}>'
+
 
 class FundNav:
     def __init__(self, date: str, value: float, increase: float):
@@ -19,13 +22,23 @@ class FundNav:
         self.value = value
         self.increase = increase
 
+    def __repr__(self):
+        return f'<{self.__class__.__name__}: {self.date}, {self.value}, {self.increase}>'
+
 
 class FundNavList:
     def __init__(self, fund_info: FundInfo, nav_list=None):
         if nav_list is None:
             nav_list = []
-        self.fund_info = fund_info
+        self.info = fund_info
         self.nav_list = nav_list
+
+    def __repr__(self):
+        info = self.info.__repr__()
+        tformat = '{:<10} {:>6} {:>6}'
+        navs = [tformat.format(i.date, i.value, i.increase) for i in self.nav_list]
+        navs = '\n'.join(navs)
+        return f'{info}\nnavs:\n{navs}'
 
     def append(self, date: str, value: float, increase: float):
         self.nav_list.append(FundNav(date, value, increase))
