12c305bc9473571bb639a10d87ea6f41dac2c4ce
yuan <yuan@preferr.com>
2022-09-05 17:31:58 +0800

    little fix

--- fundstrategy/setups.py
+++ fundstrategy/setups.py
@@ -1,8 +1,10 @@
 # coding: utf8
 import logging
 import logging.config
+import os
 
 
 def setup_logging(conf_file='logging.ini'):
+    os.makedirs('logs')
     logging.config.fileConfig(conf_file)
     logging.info(f'config log from file: {conf_file}')
