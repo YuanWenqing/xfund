558a1761288a08c38e774a132dc765cd38bec987
yuan <yuan@preferr.com>
2022-09-05 17:55:35 +0800

    setup sql

--- fundstrategy/setups.py
+++ fundstrategy/setups.py
@@ -3,8 +3,15 @@ import logging
 import logging.config
 import os
 
+from fundstrategy.core import sql_handler
+
 
 def setup_logging(conf_file='logging.ini'):
     os.makedirs('logs', exist_ok=True)
     logging.config.fileConfig(conf_file)
     logging.info(f'config log from file: {conf_file}')
+
+
+def setup_sql(mysql_url: str = 'mysql://fund:123456@localhost/fund'):
+    factory = sql_handler.ConnectionFactory.parse_url(url=mysql_url)
+    return sql_handler.SqlHandler(factory)
