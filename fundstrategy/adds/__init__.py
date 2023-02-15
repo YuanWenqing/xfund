c392b1f61b36998652284eb25f87c1a7a7cf30d9
yuan <yuan@preferr.com>
2023-02-15 17:27:02 +0800

    rename

--- fundstrategy/adds/__init__.py
+++
@@ -1,3 +0,0 @@
-# coding: utf8
-from ._by_value_drawback import AddByValueDrawback
-from ._by_value_increase import AddByValueIncrease
--- fundstrategy/strategies/__init__.py
+++ fundstrategy/strategies/__init__.py
@@ -1,6 +1,3 @@
 # coding: utf8
-from .drawback import DrawbackStrategy
-from .regular import ProfitStrategy
-from .regular import Regular
-from .take_profit import TakeProfitStrategy
-from .add_position import AddPositionByDrawbackRate
+from ._add_by_value_drawback import AddByValueDrawback
+from ._add_by_value_increase import AddByValueIncrease
