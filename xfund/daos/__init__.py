6a79b217b4bfc8bd48c5916873eeec34b42f9d92
yuan <yuan@preferr.com>
2024-08-01 16:51:20 +0800

    daos.__init__

--- xfund/daos/__init__.py
+++ xfund/daos/__init__.py
@@ -1,3 +1,9 @@
 # coding: utf8
-from .fund_dao import FundDao
-from .nav_dao import NavDao
+from ._fund_info import FundInfo
+from ._fund_info import FundInfoDao
+from ._fund_nav import FundNav
+from ._fund_nav import FundNavDao
+from ._stock_info import StockInfo
+from ._stock_info import StockInfoDao
+from ._stock_kline import StockKline
+from ._stock_kline import StockKlineDao
