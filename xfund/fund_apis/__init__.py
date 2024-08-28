9ec87bfa8d99235367daeeb58da73536d4246ca2
yuan <yuan@preferr.com>
2024-08-28 13:42:42 +0800

    SinaFinance

--- xfund/fund_apis/__init__.py
+++ xfund/fund_apis/__init__.py
@@ -1,2 +1,3 @@
 # coding: utf8
 from ._east_money import EastMoneyApi
+from ._sina_finance import SinaFinance
---
+++ xfund/fund_apis/_sina_finance.py
@@ -0,0 +1,32 @@
+# coding: utf8
+import typing
+
+import requests
+
+from xfund import daos
+from xfund.daos import StockKline
+
+
+class SinaFinance:
+    """
+    新浪财经接口
+    """
+
+    def list_stock_kline(self, code) -> typing.List[daos.StockKline]:
+        url = f'http://money.finance.sina.com.cn/quotes_service/api/json_v2.php/CN_MarketData.getKLineData?symbol={code}&scale=240&datalen=1023'
+        headers = {'content-type': 'application/json',
+                   'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:22.0) Gecko/20100101 Firefox/22.0'}
+        r = requests.get(url, headers=headers)
+        data = r.json()
+        klines = []
+        for item in data:
+            kline = StockKline(code=code,
+                               date=item['day'],
+                               open_price=item['open'],
+                               close_price=item['close'],
+                               high_price=item['high'],
+                               low_price=item['low'],
+                               volume=int(item['volume']),
+                               )
+            klines.append(kline)
+        return klines
