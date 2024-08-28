c879165400865c1902159d5266facbf54f850ea3
yuan <yuan@preferr.com>
2024-08-28 16:12:20 +0800

    littlefix

--- xfund/biz/stockbiz/_context.py
+++ xfund/biz/stockbiz/_context.py
@@ -1,9 +1,9 @@
 # coding: utf8
 from xfund.biz import beans
 from xfund.biz._biz_context import BizContext
-from xfund.biz.stockbiz import StockManager
 from xfund.biz.stockbiz._stock_info_dao import StockInfoDao
 from xfund.biz.stockbiz._stock_kline_dao import StockKlineDao
+from xfund.biz.stockbiz._stock_manager import StockManager
 
 
 class StockContext(BizContext):
