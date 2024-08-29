771226dd194c1c2454b0436d9bb8928bd871b617
yuan <yuan@preferr.com>
2024-08-29 11:17:46 +0800

    import fix

--- xfund/biz/fundbiz/_context.py
+++ xfund/biz/fundbiz/_context.py
@@ -1,7 +1,7 @@
 # coding: utf8
 from xfund.biz import beans
 from xfund.biz._biz_context import BizContext
-from xfund.biz.fundbiz import FundManager
+from xfund.biz.fundbiz._fund_manager import FundManager
 from xfund.biz.fundbiz._fund_info_dao import FundInfoDao
 from xfund.biz.fundbiz._fund_nav_dao import FundNavDao
 
